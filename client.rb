a80ee5bcc1ac3050982c1188c3629e4be244235f
QuietSup <feeling_of_infinity@outlook.com>
2025-06-12 12:58:46 +0300

    feat: optimize using threads

--- client.rb
+++ client.rb
@@ -1,3 +1,4 @@
+require 'async'
 require 'openssl'
 require 'faraday'
 
@@ -38,28 +39,28 @@ def a(value)
   Thread.new do
     puts "https://localhost:9292/a?value=#{value}"
     Faraday.get("https://localhost:9292/a?value=#{value}").body
-  end.value
+  end
 end
 
 def b(value)
   Thread.new do
     puts "https://localhost:9292/b?value=#{value}"
     Faraday.get("https://localhost:9292/b?value=#{value}").body
-  end.value
+  end
 end
 
 def c(value)
   Thread.new do
     puts "https://localhost:9292/c?value=#{value}"
     Faraday.get("https://localhost:9292/c?value=#{value}").body
-  end.value
+  end
 end
 
 # Референсное решение, приведённое ниже работает правильно, занимает ~19.5 секунд
 # Надо сделать в пределах 7 секунд
 
 def collect_sorted(arr)
-  arr.sort.join('-')
+  arr.map(&:value).sort.join('-')
 end
 
 start = Time.now
@@ -68,8 +69,9 @@ a11 = a(11)
 a12 = a(12)
 a13 = a(13)
 b1 = b(1)
+b2 = b(2)
 
-ab1 = "#{collect_sorted([a11, a12, a13])}-#{b1}"
+ab1 = "#{collect_sorted([a11, a12, a13])}-#{b1.value}"
 puts "AB1 = #{ab1}"
 
 c1 = c(ab1)
@@ -78,9 +80,9 @@ puts "C1 = #{c1}"
 a21 = a(21)
 a22 = a(22)
 a23 = a(23)
-b2 = b(2)
+b3 = b(3)
 
-ab2 = "#{collect_sorted([a21, a22, a23])}-#{b2}"
+ab2 = "#{collect_sorted([a21, a22, a23])}-#{b2.value}"
 puts "AB2 = #{ab2}"
 
 c2 = c(ab2)
@@ -89,16 +91,15 @@ puts "C2 = #{c2}"
 a31 = a(31)
 a32 = a(32)
 a33 = a(33)
-b3 = b(3)
 
-ab3 = "#{collect_sorted([a31, a32, a33])}-#{b3}"
+ab3 = "#{collect_sorted([a31, a32, a33])}-#{b3.value}"
 puts "AB3 = #{ab3}"
 
 c3 = c(ab3)
 puts "C3 = #{c3}"
 
 c123 = collect_sorted([c1, c2, c3])
-result = a(c123)
+result = a(c123).value
 time_taken = Time.now - start
 
 puts "FINISHED in #{time_taken}s."
